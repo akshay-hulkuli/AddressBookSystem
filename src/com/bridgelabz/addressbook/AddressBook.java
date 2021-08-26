3b55cf746a62c507dcaa60ca7d712a226eb836e2
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-26 18:42:38 +0530

    ADDED: AddressBook class to store PersonDetails

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.addressbook;
+
+import java.util.*; 
+
+public class AddressBook {
+	private ArrayList<PersonDetails> referenceBook = new ArrayList<PersonDetails>();
+	
+	public void addPerson() {
+		
+		PersonDetails person1 = new PersonDetails();
+		person1.setFirstName("Akshaya");
+		person1.setLastName("HP");
+		person1.setAddress("Hulkuli, Thirthahalli");
+		person1.setCity("Shimoga");
+		person1.setState("Karnataka");
+		person1.setPinCode(577415);
+		person1.setPhoneNumber("9482857473");
+		person1.setEmail("akshay@gmail.com");
+		
+		referenceBook.add(person1);
+		
+	}
+}
