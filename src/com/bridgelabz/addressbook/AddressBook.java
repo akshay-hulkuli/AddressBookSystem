edf03e190e44359308ae47df5c44f4329cfff69a
Akshaya HP <akshayhulkuli@gmail.com>
2021-08-26 18:55:22 +0530

    ADDED: method to take details from console

--- src/com/bridgelabz/addressbook/AddressBook.java
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -6,18 +6,28 @@ public class AddressBook {
 	private ArrayList<PersonDetails> referenceBook = new ArrayList<PersonDetails>();
 	
 	public void addPerson() {
-		
+		Scanner sc = new Scanner(System.in);
 		PersonDetails person1 = new PersonDetails();
-		person1.setFirstName("Akshaya");
-		person1.setLastName("HP");
-		person1.setAddress("Hulkuli, Thirthahalli");
-		person1.setCity("Shimoga");
-		person1.setState("Karnataka");
-		person1.setPinCode(577415);
-		person1.setPhoneNumber("9482857473");
-		person1.setEmail("akshay@gmail.com");
-		
+		System.out.println("Enter Person details:");
+		System.out.println("Enter firstName:");
+		person1.setFirstName(sc.next());
+		System.out.println("Enter SecondName:");
+		person1.setLastName(sc.next());
+		System.out.println("Enter Address:");
+		person1.setAddress(sc.next());
+		System.out.println("Enter City:");
+		person1.setCity(sc.next());
+		System.out.println("Enter State:");
+		person1.setState(sc.next());
+		System.out.println("Enter Pin code:");
+		person1.setPinCode(sc.nextInt());
+		System.out.println("Enter Phone nmber:");
+		person1.setPhoneNumber(sc.next());
+		System.out.println("Enter email:");
+		person1.setEmail(sc.next());
+		sc.close();
 		referenceBook.add(person1);
 		
 	}
+	
 }
